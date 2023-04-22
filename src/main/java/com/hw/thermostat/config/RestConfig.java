ba61c22c6f28a1b28b074c358b1913a565a3d4bb
takoba18 <takoba18@freeuni.edu.ge>
2023-04-23 03:04:41 +0400

    hopefully final

---
+++ src/main/java/com/hw/thermostat/config/RestConfig.java
@@ -0,0 +1,17 @@
+package com.hw.thermostat.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .additionalMessageConverters(new MappingJackson2HttpMessageConverter())
+                .build();
+    }
+}
